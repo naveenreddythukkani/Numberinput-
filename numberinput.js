976d132fa324fbf7a3965a9330e2634f17f3b6c1
Naveen Kumar <naveenkumar.thukkani@mobigesture.com>
2018-01-25 19:16:24 +0530

    only numbers we can enter into input field

--- numberinput.js
+++ numberinput.js
@@ -1,27 +1,27 @@
-export {
-    ctrlDown:false
-    ctrlKey:17
-    vKey:86
-    cKey:67
-    cmdKey:91
-   keyup:function(event) {
-       if ((event.keyCode == ctrlKey)|| (event.keyCode == cmdKey)){
-           ctrlDown = false;
-         }
-   },
-   keydown:function(event) {
-        if ((event.keyCode == ctrlKey) || (event.keyCode == cmdKey)){
+export numberinput = {
+    ctrlDown: false,
+    ctrlKey: 17,
+    vKey: 86,
+    cKey: 67,
+    cmdKey: 91,
+    keyup: function(event) {
+        if ((event.keyCode == ctrlKey) || (event.keyCode == cmdKey)) {
+            ctrlDown = false;
+        }
+    },
+    keydown: function(event) {
+        if ((event.keyCode == ctrlKey) || (event.keyCode == cmdKey)) {
             ctrlDown = true;
-          }
-          if ((event.keyCode < 48 || event.keyCode > 57)
-           && (event.keyCode < 96 || event.keyCode > 105)
-           && (event.keyCode != 8)
-           && (event.keyCode != 46)
-           && (event.keyCode != 37)
-           && (event.keyCode != 39)
-           &&(event.keyCode != 9)
-           &&!(ctrlDown && (event.keyCode == 86 || event.keyCode == 88 || event.keyCode == 67 || event.keyCode == 82))) {
-                 event.preventDefault();
-          }
-   }
+        }
+        if ((event.keyCode < 48 || event.keyCode > 57) &&
+            (event.keyCode < 96 || event.keyCode > 105) &&
+            (event.keyCode != 8) &&
+            (event.keyCode != 46) &&
+            (event.keyCode != 37) &&
+            (event.keyCode != 39) &&
+            (event.keyCode != 9) &&
+            !(ctrlDown && (event.keyCode == 86 || event.keyCode == 88 || event.keyCode == 67 || event.keyCode == 82))) {
+            event.preventDefault();
+        }
+    }
 }
